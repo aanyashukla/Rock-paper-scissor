f9ab4a20f2e078a4dba396abf6c6e7e17871987c
Aanya <aanyashukla67@gmail.com>
2024-10-03 21:10:50 +0530

    Added a new feature where by clicking key 'r', 'p' and  's' anywhere on the website will select 'rock', 'paper', and 'scissors' and play the game.

--- 10-rock-paper-scissors.js
+++ 10-rock-paper-scissors.js
@@ -32,6 +32,16 @@ function autoplay(){
   }
 }
 
+document.body.addEventListener('keydown', (event) => {
+  if(event.key === 'r'){
+    playGame('rock');
+  }else if(event.key === 'p'){
+    playGame('paper');
+  }else if(event.key === 's'){
+    playGame('scissors');
+  }
+});
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
